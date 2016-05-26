11a58fec24f25359409531494592cf19cc92094d
Matt Ball <mattdrivendev@gmail.com>
2016-05-26 13:56:41 +0100

    Adds Gareth's point-on-circle math to a helpers module

---
+++ bbb.game/Helpers.fs
@@ -0,0 +1,13 @@
+﻿[<AutoOpen>]
+module Helpers
+
+open System
+open Microsoft.Xna.Framework
+
+
+/// Point on Circle - returns the position given the radius from the centre
+/// of a circle and an angle. Can be used to move objects "around" another.
+let poc (centre:Vector2) radius angle = 
+  new Vector2(
+    x = (centre.X + float32 (radius * (Math.Cos(angle)))),
+    y = (centre.Y + float32 (radius * (Math.Sin(angle)))))
